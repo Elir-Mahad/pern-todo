81fd9a6c9f82b0d75dbe5aac28e22b3a27098d8b
elir-mahad <mmahad280@gmail.com>
2022-03-18 13:01:22 -0400

    enhance(index): add express listening function

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 const express = require("express");
+// import express
 const app = express();
+// run it throught the const app
+
+app.listen(5000, () => {
+  // if the server starts
+  console.log("server has started on port 5000");
+  //   show this in the console log
+});
