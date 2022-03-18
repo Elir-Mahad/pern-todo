7f4d3aa7f3728277f73757d51b3e4eb7331ca984
elir-mahad <mmahad280@gmail.com>
2022-03-18 14:23:59 -0400

    feat(server): create databasefile with next steps overview

---
+++ server/database.sql
@@ -0,0 +1,15 @@
+-- Your postgresql will contain multiple databases. 
+-- Each database will contain a bunch of tables. 
+-- Each table will contain, data that's relevant to that table.
+
+-- In this file we are going to execute the following tasks:
+-- 1. Create a database called todo_database
+-- 2. Inside todo_database, create a table called todo
+-- 3. Define a schema; this is important because we don't want to put anything inside our database. 
+--    We want to be specific about what will go in the database.
+
+-- We will use the following commands to move around the database:
+-- \l (this will list all databases in postgresql)
+-- \c (move inside a database)
+-- \dt (show a table in database)
+
