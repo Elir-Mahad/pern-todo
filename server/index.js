55d885a142194a9d8758d18582a5061e61a45c47
elir-mahad <mmahad280@gmail.com>
2022-03-18 13:13:41 -0400

    enhance(index): import cors and run it via express use function

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
-// import express
+// import express package
+const cors = require("cors");
+// import cors package
+
 const app = express();
-// run it throught the const app
+// run express through the const app
+
+//! middleware
+app.use(cors());
 
 app.listen(5000, () => {
   // if the server starts
   console.log("server has started on port 5000");
-  //   show this in the console log
+  // show this in the console log
 });
