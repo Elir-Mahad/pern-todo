6a644a1eedfc7c6576c81bbfad6be690686cbe71
elir-mahad <mmahad280@gmail.com>
2022-03-18 14:07:35 -0400

    docs(index): add comments explaning middleware functions

--- server/index.js
+++ server/index.js
@@ -7,8 +7,16 @@ const app = express();
 // run express through the const app
 
 //! middleware
+// middleware refers to the methods/functions/operations
+// that are called BETWEEN
+// processing the Request
+// and sending the Response
+// in your application method
+
 app.use(cors());
+// this is a method inbuilt in express to allow cross origin requests
 app.use(express.json());
+// this is a method inbuilt in express to recognize the incoming Request Object as a JSON Object
 
 app.listen(5000, () => {
   // if the server starts
